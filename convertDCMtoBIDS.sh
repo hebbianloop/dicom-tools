be12b041e8ac57620b11ebba2643da5335acbd73
Shad <se394@georgetown.edu>
2018-04-18 15:38:55 -0400

    removed session for edith

--- convertDCMtoBIDS.sh
+++ convertDCMtoBIDS.sh
@@ -1,6 +1,5 @@
 DICOMDIR=/Volumes/Anvil/sync/mri.raw/adolescent-development-study/dicoms
 BIDSDIR=/Volumes/Anvil/sync/datasets/ads.bids.dataset
-sessions="001 002 003"
 # Get dependencies (make into function later...)
 if [ -z $(which dcm2niix) ]; then
     if [ $(uname) = 'Darwin' ]; then
@@ -31,13 +30,12 @@ if [ -z $(which parallel) ]; then
     	echo "GNU parallel does not exist on this system.. this installation is complicated.. please download->build first then run this again.. sorry! exiting.." && exit
     fi
 fi
-# Loop across sessions and subjects then do tasks
-for session in ${sessions}; do
-	subjects=$(find ${DICOMDIR}/${session} -type d -d 1 | sed 's|/| |g' | awk '{print $NF}')
+# Loop across subjects then do tasks
+	subjects=$(find ${DICOMDIR}/ -type d -d 1 | sed 's|/| |g' | awk '{print $NF}')
 	##  build jsons for each subject
 	for subject in ${subjects}; do
-		parallel -k --link --bar dcm2niix -b o -ba n ${DICOMDIR}/${session}/${subject}/{} ::: $(find ${DICOMDIR}/${session}/${subject} -type d -d 1 | sed 's|/| |g' | awk '{print $NF}') 
+		parallel -k --link --bar dcm2niix -b o -ba n ${DICOMDIR}/${subject}/{} ::: $(find ${DICOMDIR}/${subject} -type d -d 1 | sed 's|/| |g' | awk '{print $NF}') 
 	done
 	## now convert
-	parallel -k --link --bar dcm2bids -d ${DICOMDIR}/${session}/{}/* -p {} -s ${session} -c /Volumes/Anvil/sync/mri.raw/adolescent-development-study/config.json -o ${BIDSDIR} ::: $(find ${DICOMDIR}/${session} -type d -d 1 | sed 's|/| |g' | awk '{print $NF}')	
+	parallel -k --link --bar dcm2bids -d ${DICOMDIR}/{}/* -p {} -s ses-01 -c /Volumes/Anvil/sync/mri.raw/adolescent-development-study/config.json -o ${BIDSDIR} ::: $(find ${DICOMDIR}/ -type d -d 1 | sed 's|/| |g' | awk '{print $NF}')	
 done
